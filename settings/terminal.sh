d66d72d98683ac3421d733b8f775a7816d3fb59d
Mark Boltengagen <m.boltengagen@gmail.com>
2026-01-07 19:16:33 +0100

    update

--- settings/terminal.sh
+++ settings/terminal.sh
@@ -3,6 +3,7 @@
 # Make alacritty default terminal emulator
 sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
 # Alacritty configuration
+mkdir -p ~/.local/share/alacritty/
 cp ~/.local/share/ubuntu_setup/configs/alacritty/alacritty.toml ~/.local/share/alacritty/
 
 # Tmux configuration
