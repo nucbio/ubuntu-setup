1f9b767e952ae689e16396c27dc8eb71f59c3351
Mark Boltengagen <m.boltengagen@gmail.com>
2026-04-20 13:55:16 +0200

    nvm

--- apps_install/agents.sh
+++ apps_install/agents.sh
@@ -5,6 +5,9 @@ curl -fsSL https://claude.ai/install.sh | bash
 
 # Gemini
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
-source ~/.bashrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 nvm install node
 npm install -g @google/gemini-cli
