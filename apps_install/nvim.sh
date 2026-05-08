24195f9c6e49e6530cc9a087fc1c6e8dcc420955
Mark Boltengagen <m.boltengagen@gmail.com>
2026-05-08 07:54:28 +0200

    nvim rerun

--- apps_install/nvim.sh
+++ apps_install/nvim.sh
@@ -4,5 +4,9 @@ sudo snap install nvim  --classic
 
 # Nvim configuration
 #Install from github
-git clone https://github.com/nucbio/nvim-config.git $HOME/.config/nvim
+if [ -d "$HOME/.config/nvim/.git" ]; then
+    git -C "$HOME/.config/nvim" pull
+else
+    git clone https://github.com/nucbio/nvim-config.git "$HOME/.config/nvim"
+fi
 #cp -r ~/.local/share/ubuntu-setup/configs/nvim ~/.config/
