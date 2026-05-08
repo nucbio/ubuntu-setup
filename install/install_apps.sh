895b910af6337d9afe25796ab764696ef7821379
Mark Boltengagen <m.boltengagen@gmail.com>
2026-05-08 12:45:52 +0200

    install_apps primary first

--- install/install_apps.sh
+++ install/install_apps.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # Install Apps
+
+# Primary Install
+for install_app in ${UBUNTU_SETUP_DIR}/apps_install/primary/*.sh
+  do 
+    source $install_app
+  done
+
+# The rest apps
 for install_app in ${UBUNTU_SETUP_DIR}/apps_install/*.sh
   do 
     source $install_app
