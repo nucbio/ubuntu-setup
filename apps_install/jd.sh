70b953bcac3c72f3d055ffa9a75531a4d03f7efc
Mark Boltengagen <m.boltengagen@gmail.com>
2026-05-08 08:38:50 +0200

    jd2 skip if installed

--- apps_install/jd.sh
+++ apps_install/jd.sh
@@ -2,9 +2,17 @@
 
 ## JDownloader
 if [[ "$INSTALL_OPT" == "Home" ]]; then
-  mkdir -p ~/tmp && cd ~/tmp
-  wget http://installer.jdownloader.org/JD2Setup_x64.sh
-  chmod +x JD2Setup_x64.sh
-  ./JD2Setup_x64.sh
-fi
+    # Skip installation if already installed
+    if find "$HOME" -maxdepth 3 -type d -iname "jd2" | grep -q .; then
+        echo "JDownloader already installed. Skipping."
+    else
+        mkdir -p ~/tmp
+        cd ~/tmp || exit 1
 
+        wget -O JD2Setup_x64.sh \
+            http://installer.jdownloader.org/JD2Setup_x64.sh
+
+        chmod +x JD2Setup_x64.sh
+        ./JD2Setup_x64.sh
+    fi
+fi
